d11a9f24a0c9965a20197062fa042aca31f77e76
salat-23 <81823691+salat-23@users.noreply.github.com>
2021-08-06 23:05:09 +0300

    Update Main.java

--- src/main/java/GamBot/Main.java
+++ src/main/java/GamBot/Main.java
@@ -41,10 +41,6 @@ public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws LoginException
     {
-        if (args.length < 1) {
-            System.out.println("You have to provide a token as first argument!");
-            System.exit(1);
-        }
         // args[0] should be the token
         // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
         // All other events will be disabled.
