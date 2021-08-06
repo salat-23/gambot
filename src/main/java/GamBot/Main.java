b3cea1bad78e8b82c582609480bd0024e17ea88c
salat-23 <81823691+salat-23@users.noreply.github.com>
2021-08-06 22:50:12 +0300

    Update Main.java

--- src/main/java/GamBot/Main.java
+++ src/main/java/GamBot/Main.java
@@ -11,6 +11,7 @@ import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.internal.JDAImpl;
 import org.jetbrains.annotations.NotNull;
 
@@ -22,14 +23,14 @@ import java.util.stream.Stream;
 
 public class Main extends ListenerAdapter {
 
-    public static void main(String[] args) throws LoginException {
+    /*public static void main(String[] args) throws LoginException {
 
         JDABuilder jdaBuilder = JDABuilder.createDefault(System.getenv("TOKEN"));
 
         jdaBuilder.setActivity(Activity.watching("you"));
 
         jdaBuilder.build();
-    }
+    }*/
 
     /*@Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
@@ -37,6 +38,21 @@ public class Main extends ListenerAdapter {
 
         event.getChannel().sendMessage("The message was sent by " + event.getAuthor() + "in " + event.getChannel().toString()).queue();
     }*/
+
+    public static void main(String[] args) throws LoginException
+    {
+        if (args.length < 1) {
+            System.out.println("You have to provide a token as first argument!");
+            System.exit(1);
+        }
+        // args[0] should be the token
+        // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
+        // All other events will be disabled.
+        JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+                .addEventListeners(new Main())
+                .setActivity(Activity.playing("Type !ping"))
+                .build();
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
