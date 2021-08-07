47e18831dd09413b9b60adc56c50ae8eed3ff721
salat-23 <81823691+salat-23@users.noreply.github.com>
2021-08-07 11:20:34 +0300

    hello

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,31 +7,18 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
+import java.util.Scanner;
 
 public class Main extends ListenerAdapter {
 
-    /*public static void main(String[] args) throws LoginException {
-
-        JDABuilder jdaBuilder = JDABuilder.createDefault(System.getenv("TOKEN"));
-
-        jdaBuilder.setActivity(Activity.watching("you"));
-
-        jdaBuilder.build();
-    }*/
-
-    /*@Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        Message message = event.getMessage();
-
-        event.getChannel().sendMessage("The message was sent by " + event.getAuthor() + "in " + event.getChannel().toString()).queue();
-    }*/
-
     public static void main(String[] args) throws LoginException
     {
-        // args[0] should be the token
-        // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
-        // All other events will be disabled.
-        JDABuilder.createLight(System.getenv("TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+        Scanner scanner = new Scanner(System.in);
+
+
+        String token = scanner.nextLine();
+
+        JDABuilder.createLight(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
                 .addEventListeners(new Main())
                 .setActivity(Activity.playing("Type !ping"))
                 .build();
